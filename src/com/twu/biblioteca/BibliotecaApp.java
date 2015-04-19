bf244a4444b225431b08fe78f136e116d9067b50
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2015-04-19 19:51:03 -0300

    passing welcome test

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,4 +5,12 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+
+    BibliotecaApp(){
+        this.run();
+    }
+
+    private void run(){
+        System.out.println("Welcome!");
+    }
 }
