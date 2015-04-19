ab3f440ddb639fb33b7aa985bf7343eaae1e2936
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2015-04-19 19:48:52 -0300

    added welcome message test

---
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by bonifacio on 4/19/15.
+ */
+
+public class BibliotecaTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+
+    @Test
+    public void testWelcomingMessage() {
+        BibliotecaApp biblioteca = new BibliotecaApp();
+
+        assertEquals("Welcome!", outContent.toString());
+    }
+
+}
