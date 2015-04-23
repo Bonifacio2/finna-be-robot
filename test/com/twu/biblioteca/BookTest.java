6ade0040d06f079cf41396f5c7297264b1bc065e
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2015-04-23 15:38:24 -0300

    added test for getTitle

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -1,7 +1,31 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by bonifacio on 4/23/15.
  */
 public class BookTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+
+    @Test
+    public void testGetTitle() {
+        Book book = new Book("how to trololo", "me", 2015);
+        assertEquals(book.getTitle(), "how to trololo");
+    }
 }
