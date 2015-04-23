39246386287a41cfa347e9ada34f899a3ebf3733
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2015-04-23 15:39:13 -0300

    passing test fot getTitle

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -4,4 +4,16 @@ package com.twu.biblioteca;
  * Created by bonifacio on 4/23/15.
  */
 public class Book {
+
+    String title;
+    String author;
+    int year;
+
+    Book(String title, String author, int year){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
 }
