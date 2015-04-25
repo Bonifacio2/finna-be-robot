3358696d112d89f983eff7f0c476c98bddb27985
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2015-04-25 20:50:50 -0300

    passing tests for Movie class

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by bonifacio on 4/25/15.
+ */
+public class Movie {
+
+    private String title;
+    private int year;
+    private String director;
+    private int rating;
+
+    Movie(String title, int year, String director, int rating){
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public String getDirector(){
+        return this.director;
+    }
+
+}
