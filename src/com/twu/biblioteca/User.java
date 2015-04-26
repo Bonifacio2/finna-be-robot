d1362f68d31e733e44f73a49b3210262d77661d0
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2015-04-26 17:40:00 -0300

    passing tests for User class

--- src/com/twu/biblioteca/User.java
+++ src/com/twu/biblioteca/User.java
@@ -4,4 +4,28 @@ package com.twu.biblioteca;
  * Created by bonifacio on 4/25/15.
  */
 public class User {
+
+    private String libraryNumber;
+    private String password;
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+
+    public User(String libraryNumber, String password, String name, String email, String phoneNumber){
+        this.libraryNumber = libraryNumber;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLibraryNumber(){
+        return this.libraryNumber;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
 }
